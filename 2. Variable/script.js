4ca900236110f1a40ab1aefc02ec94fac8f243ea
D-Karak <dibyendu.karak05@gmail.com>
2024-09-04 21:14:42 +0530

    JavaScript

---
+++ 2. Variable/script.js
@@ -0,0 +1,10 @@
+//Create variables to store your name, age, and a boolean representing if you are astudent. Log these values to the console.//
+
+var name = "Dibyendu"; //here keyword var means variable 
+var age = 22; //name,age,student are variable names
+var student = true;//"Dibyendu",22,true is data types
+
+
+console.log(name);//This how to print variable
+console.log(age);
+console.log(student);
