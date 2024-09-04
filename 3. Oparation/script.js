ee81407a37bacf6b4970e4784dfe05e768fbef51
D-Karak <dibyendu.karak05@gmail.com>
2024-09-04 21:44:06 +0530

    JavaScript

---
+++ 3. Oparation/script.js
@@ -0,0 +1,6 @@
+//Write a program to calculate the area of a rectangle using variables for width and height.
+var width = 10;
+var height = 5;
+var area = width*height;
+
+console.log("The area of the rectangle which has "+width+" cm of width and "+height+" cm of height is "+area);//how to write variable in console log
