be3dd758f676eae7c0bfb99ce101da5fbe1c79e6
D-Karak <dibyendu.karak05@gmail.com>
2024-09-05 21:57:46 +0530

    JavaScript

---
+++ 10. Reminder calculator/script.js
@@ -0,0 +1,9 @@
+//Write a program that calculates the remainder when one number is divided by another.
+var dividend = 16;//dividend/divisor
+var divisor = 2;
+var remainder = dividend % divisor;
+if(divisor===0){
+    console.log("0 can not be divided by any number");
+    
+}
+else{console.log("The remainder is " + remainder + " when " + dividend + " is devided by " + divisor);}
