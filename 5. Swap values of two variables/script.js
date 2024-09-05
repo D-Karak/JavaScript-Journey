bcc3375bdb6f54b59e5b8cafd92a41cda5077210
D-Karak <dibyendu.karak05@gmail.com>
2024-09-05 12:13:07 +0530

    JavaScript

--- 5. Swap values of two variables/script.js
+++ 5. Swap values of two variables/script.js
@@ -1,6 +1,6 @@
 //Write a program to swap the values of two variables.
 
-var a=5;
-var b=10;
-a=b;
-console.log("The value of a is: "+a);
+var a = 5;
+var b = 10;
+a = b;
+console.log("The value of a is: " + a);
---
+++ 6. Basic operations/script.js
@@ -0,0 +1,11 @@
+//Write a program that takes two numbers performs addition, subtraction, multiplication, and division
+var num1 = 15;
+var num2 = 3;
+var add = num1 + num2;
+var sub = num1 - num2;
+var mult = num1 * num2;
+var div = num1 / num2;
+console.log("The addition of " + num1 + " and " + num2 + " is " + add);
+console.log("The substraction of " + num1 + " and " + num2 + " is " + sub);
+console.log("The multiplication of " + num1 + " and " + num2 + " is " + mult);
+console.log("The division of " + num1 + " and " + num2 + " is " + div);
