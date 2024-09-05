13e766b0c437297d83febac36678e6dea5982674
D-Karak <dibyendu.karak05@gmail.com>
2024-09-05 22:11:05 +0530

    JavaScript

---
+++ 11. Assign Grades by else if/script.js
@@ -0,0 +1,15 @@
+// Write a program that assigns a letter grade (A, B, C, D, or F) based on a student's score.
+var marks=20;
+
+if (marks>=90) {
+    console.log("Excelent You got an A+");
+}else if(marks>=70){
+    console.log("Very Good You got an A");
+}else if(marks>=60){
+    console.log("Good You got a B+");
+}else if(marks>=50){
+    console.log("You got a B");
+}else if(marks>=30){
+    console.log("You got a c");
+}else{console.log("You have Failed");
+}
