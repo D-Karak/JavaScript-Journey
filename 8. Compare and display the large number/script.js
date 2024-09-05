a3990ef60fe1eab3984bf1d91cb859d30ddac892
D-Karak <dibyendu.karak05@gmail.com>
2024-09-05 15:22:55 +0530

    JavaScript

--- 8. Compare and display the large number/script.js
+++ 8. Compare and display the large number/script.js
@@ -6,7 +6,7 @@
   if(num1>num2){
     console.log(num1+" is bigger than "+num2); 
   }
-  else if(num1===num2){
+  else if(num1==num2){
     console.log(num1+" is equal to "+num2); 
   }
   else{console.log(num2+" is bigger than "+num1); }
---
+++ 9. Eligiblity condition checker/script.js
@@ -0,0 +1,9 @@
+//Create a program that checks if a person is eligible to vote (age 18 or older).
+var age=parseInt (prompt("Enter your age"))
+
+if(age>=18){
+    console.log("Congratulation! you are eligible to vote"); 
+}
+else{
+    console.log("Sorry! you are not eligible to vote");
+}
