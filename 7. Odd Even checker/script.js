d8f5d06e745a65dbbec59bbbfe3d6ff4a97aa37d
D-Karak <dibyendu.karak05@gmail.com>
2024-09-05 13:03:16 +0530

    JavaScript

---
+++ 7. Odd Even checker/script.js
@@ -0,0 +1,10 @@
+//Create a program that checks if a given number is even or odd
+var number = Number(prompt("Enter the number: "));
+ 
+//statement for even and odd number checker
+if(number%2===0){
+    console.log("The number "+number+" is an Even number.");
+}
+else{
+    console.log("The number "+number+" is an Odd number.");
+}
