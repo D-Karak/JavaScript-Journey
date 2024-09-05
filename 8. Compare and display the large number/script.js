002788f1f0fde21e958134622db02ac81aed6667
D-Karak <dibyendu.karak05@gmail.com>
2024-09-05 13:25:30 +0530

    JavaScript

---
+++ 8. Compare and display the large number/script.js
@@ -0,0 +1,12 @@
+//Write a program that compares two numbers and displays the larger one.
+ var num1=10;
+ var num2=15;
+
+ //conditional statement for the equation
+  if(num1>num2){
+    console.log(num1+" is bigger than "+num2); 
+  }
+  else if(num1===num2){
+    console.log(num1+" is equal to "+num2); 
+  }
+  else{console.log(num2+" is bigger than "+num1); }
