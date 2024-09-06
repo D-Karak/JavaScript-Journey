221db41a6dc957b347fee88f97f85f8faa9e15b4
D-Karak <dibyendu.karak05@gmail.com>
2024-09-06 12:57:31 +0530

    JavaScript

---
+++ 13. Using else-if for finding the type of integer/script.js
@@ -0,0 +1,13 @@
+//Write a program that checks if a number is positive, negative, or zero using if-else statements.
+
+var number;
+number= -35;
+
+if (number>0) {
+    console.log("The number is positive");
+    
+} else if(number<0){
+    console.log("The number is negative");
+}else if(number===0){
+    console.log("This is a 0");
+}
