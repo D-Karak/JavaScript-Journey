2d830551a9a5db1c49808f924197cf7d9e4f5e7f
D-Karak <dibyendu.karak05@gmail.com>
2024-09-06 13:27:35 +0530

    JavaScript

---
+++ 16. Multiplication table of given number(for loop)/script.js
@@ -0,0 +1,8 @@
+//Write a program to print the multiplication table for a given number
+
+var tableOf;
+tableOf=2;
+
+for (let i = 1; i <= 10; i++){
+    console.log(+tableOf+" x "+i+" = "+tableOf*i);
+} 
