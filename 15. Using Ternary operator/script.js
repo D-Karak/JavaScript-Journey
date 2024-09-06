79346228b98337984fcc7c2d493b99a3a56c4b9f
D-Karak <dibyendu.karak05@gmail.com>
2024-09-06 13:20:20 +0530

    JavaScript

---
+++ 15. Using Ternary operator/script.js
@@ -0,0 +1,6 @@
+//Write a program that uses the ternary operator to check if a number is even or odd.
+var number;
+number=23;
+var output = (number % 2 == 0) ? console.log("This is an Even number") : console.log("This is an odd number");
+
+//structure--> condition ? (true expression) : (false expression)
