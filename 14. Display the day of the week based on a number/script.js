703fe32cc3a0e0c22525305f345261cb44f8c4d6
D-Karak <dibyendu.karak05@gmail.com>
2024-09-06 13:07:58 +0530

    JavaScript

---
+++ 14. Display the day of the week based on a number/script.js
@@ -0,0 +1,29 @@
+//Create a program that determines the day of the week based on a number input (1 for Monday, 2 for Tuesday, etc.).
+
+var dayOfWeek;
+dayOfWeek = 7;
+switch (dayOfWeek) {
+    case 1:
+        console.log("Today is Monday")
+        break;
+    case 2:
+        console.log("Today is Tuesday")
+        break;
+    case 3:
+        console.log("Today is Wednesday")
+        break;
+    case 4:
+        console.log("Today is Thursday")
+        break;
+    case 5:
+        console.log("Today is Friday")
+        break;
+    case 6:
+        console.log("Today is Saturday")
+        break;
+    case 7:
+        console.log("Today is Sunday")
+        break;
+    default:
+        break;
+}
