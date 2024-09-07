19011d90cad91d1d6995af657d721d996ce47af3
D-Karak <dibyendu.karak05@gmail.com>
2024-09-07 14:12:23 +0530

    JavaScript

---
+++ 19. Factorial of a given number/script.js
@@ -0,0 +1,9 @@
+//Create a program that finds the factorial of a given number using a for loop.
+var factorial=4;
+var startValue=1;
+for(let i=1;i<=factorial;i++){
+    startValue*=i//startValue=startValue*i
+    console.log(startValue);
+    
+}
+//console.log(result);
